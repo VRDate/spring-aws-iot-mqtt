7814f9dcd317f4d7f83e9227f409f445aa5e749e
Mambo <kdevkr@gmail.com>
2021-08-08 13:24:08 +0900

    init

---
+++ src/main/java/com/example/demo/AwsIotMqttApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsIotMqttApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsIotMqttApplication.class, args);
+    }
+
+}
